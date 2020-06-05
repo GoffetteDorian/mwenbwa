8cef3d999d3ee373ed0d552493d85194065c6199
Mustafa <m.devbe@gmail.com>
2020-06-05 09:39:06 +0200

    adding marker cluster to the map

---
+++ src/client/components/Slide.js
@@ -0,0 +1,15 @@
+// import React, { Component } from 'react';
+// class Slide extends Component {
+//     render() {
+//         return (
+//             <div className={"slide-main"}>
+//                 <div className={"slide-head"}>
+//                     <button className={"lb-button"}> <h3> leaderboard </h3> </button>
+//                     <span> </span>
+//                     <button> <h3>gamelog</h3> </button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+// export default Slide;
--- src/client/components/hello.js
+++ src/client/components/hello.js
@@ -7,12 +7,36 @@
  */
 
 import * as React from "react";
+// import Slide from "./slide";
+import treeIcon from "../../images/tree-2.png";
+import leafIcon from "../../images/leaf.png";
+import menuIcon from "../../images/open-menu.png";
+import gearIcon from "../../images/gear.png";
 
 const HelloWorld = () => (
     <div>
-        <h1>{"Hello, world!"}</h1>
-        <hr />
-        <small>{"becode/trouvkach"}</small>
+        <div className={"nav-main"}>
+            <div className={"icons-container"}>
+                <img className={"icons"} src={treeIcon} />
+                <span className={"counter"}>{12}</span>
+                <img className={"icons"} src={leafIcon} />
+                <span className={"counter"}>{232}</span>
+            </div>
+
+            <div className={"timers"}>
+                <span className={"timer"}> {59} </span>
+                <span className={"timer"}> {15} </span>
+            </div>
+
+            <div className={"test"}>
+                <img className={"gearIcon"} src={gearIcon} />
+                <img className={"menuIcon"} src={menuIcon} />
+            </div>
+        </div>
+
+        {/* <div>
+            <Slide />
+        </div> */}
     </div>
 );
 
