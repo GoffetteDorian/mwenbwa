2d94c3f2d8ba2d8a1f59cc6a49e42b1f034bcc31
GoffetteDorian <goffettedorian@gmail.com>
2020-06-11 15:51:46 +0200

    Install jwt & body-parser

--- src/server/index.js
+++ src/server/index.js
@@ -11,6 +11,8 @@ import path from "path";
 
 import db from "./db/db";
 
+const jwt = require("express-jwt");
+
 const users = require("./routes/user-routes.js");
 const trees = require("./routes/tree-routes.js");
 
@@ -21,6 +23,13 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "../../bin/client")));
 
 db.on("error", console.error.bind(console, "mongodb connection error!"));
+
+app.get("/protected", jwt({secret: "shared-secret"}), (req, res) => {
+    if (!req.user.admin) {
+        return res.sendStatus(401);
+    }
+    return res.sendStatus(200);
+});
 
 app.use("/api/users", users);
 app.use("/api/trees", trees);
