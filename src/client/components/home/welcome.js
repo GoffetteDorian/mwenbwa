4de46d7bcda7682d4ed65e56d0cff50e9048e330
Mustafa <m.devbe@gmail.com>
2020-06-23 10:08:11 +0200

    fixing welcome conflict

--- src/client/components/home/welcome.js
+++ src/client/components/home/welcome.js
@@ -1,21 +1,6 @@
 /* eslint-disable react/button-has-type */
 
 import * as React from "react";
-<<<<<<< HEAD:src/client/components/home/welcome.js
-import About from "../../../images/about.png";
-import Mwenbwatree from "../../../images/tree-3.png";
-
-const Welcome = () => (
-    <div className={"mwenbwa-main"}>
-        <div className={"mwenbwa-container"}>
-            <div className={"mwenbwa-name"}>
-                <h1 className={"mwenbwa"}> MWENBWA</h1>
-                <h2> liège </h2>
-            </div>
-            <div className={"mwenbwa-button"}>
-                <button className={"mwenbwa-signin"}> Sign In </button>
-                <button> Sign Up </button>
-=======
 
 const Welcome = () => (
     <div className={"mwenbwa-main"}>
@@ -29,7 +14,6 @@ const Welcome = () => (
                     <button className={"mwenbwa-signin"}>{" Sign In "}</button>
                     <button>{" Sign Up "}</button>
                 </div>
->>>>>>> ajouts-mustafa:src/client/components/welcome.js
             </div>
         </div>
     </div>
